fc6c2d3c0b4a621052613cbc5ea3bc65596d109c
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-05-28 20:30:13 +0100

    all tests pass

--- part_b_sports_team/specs/sports_team_spec.rb
+++ part_b_sports_team/specs/sports_team_spec.rb
@@ -3,30 +3,84 @@ require_relative("../sports_team")
 
 class TestTeam < MiniTest::Test
 
+  # def test_get_name
+  #   team = Team.new("Liverpool", ["Karius", "van Dijk", "Henderson", "Mane", "Salah"], "Klopp")
+  #   assert_equal("Liverpool", team.get_name)
+  # end
+  #
+  # def test_get_players__array
+  #   team = Team.new("Liverpool", ["Karius", "van Dijk", "Henderson", "Mane", "Salah"], "Klopp")
+  #   assert_equal(["Karius", "van Dijk", "Henderson", "Mane", "Salah"], team.get_players)
+  # end
+  #
+  # def test_get_players__number
+  #   team = Team.new("Liverpool", ["Karius", "van Dijk", "Henderson", "Mane", "Salah"], "Klopp")
+  #   assert_equal(5, team.get_players.count)
+  # end
+  #
+  # def test_get_coach
+  #   team = Team.new("Liverpool", ["Karius", "van Dijk", "Henderson", "Mane", "Salah"], "Klopp")
+  #   assert_equal("Klopp", team.get_coach)
+  # end
+
   def test_get_name
     team = Team.new("Liverpool", ["Karius", "van Dijk", "Henderson", "Mane", "Salah"], "Klopp")
-    assert_equal("Liverpool", team.get_name)
+    assert_equal("Liverpool", team.name)
   end
 
   def test_get_players_array
     team = Team.new("Liverpool", ["Karius", "van Dijk", "Henderson", "Mane", "Salah"], "Klopp")
-    assert_equal(["Karius", "van Dijk", "Henderson", "Mane", "Salah"], team.get_players)
+    assert_equal(["Karius", "van Dijk", "Henderson", "Mane", "Salah"], team.players)
   end
 
   def test_get_players__number
     team = Team.new("Liverpool", ["Karius", "van Dijk", "Henderson", "Mane", "Salah"], "Klopp")
-    assert_equal(5, team.get_players.count)
+    assert_equal(5, team.players.count)
   end
 
   def test_get_coach
     team = Team.new("Liverpool", ["Karius", "van Dijk", "Henderson", "Mane", "Salah"], "Klopp")
-    assert_equal("Klopp", team.get_coach)
+    assert_equal("Klopp", team.coach)
   end
 
   def test_set_coach
     team = Team.new("Liverpool", ["Karius", "van Dijk", "Henderson", "Mane", "Salah"], "Klopp")
-    team.set_coach("Dalglish")
-    assert_equal("Dalglish", team.get_coach)
+    team.coach = "Dalglish"
+    assert_equal("Dalglish", team.coach)
+  end
+
+  def test_add_new_player
+    team = Team.new("Liverpool", ["Karius", "van Dijk", "Henderson", "Mane", "Salah"], "Klopp")
+    team.add_new_player("Alexander-Arnold")
+    assert_equal(6, team.players.count)
+  end
+
+  def test_player_in_team__present
+    team = Team.new("Liverpool", ["Karius", "van Dijk", "Henderson", "Mane", "Salah"], "Klopp")
+    assert_equal(true, team.player_in_team?("Mane"))
+  end
+
+  def test_player_in_team__absent
+    team = Team.new("Liverpool", ["Karius", "van Dijk", "Henderson", "Mane", "Salah"], "Klopp")
+    assert_equal(false, team.player_in_team?("Ramos"))
+  end
+
+  def test_points__win
+    team = Team.new("Liverpool", ["Karius", "van Dijk", "Henderson", "Mane", "Salah"], "Klopp")
+    team.result("win")
+    assert_equal(3, team.points)
+  end
+
+  def test_points__lose
+    team = Team.new("Liverpool", ["Karius", "van Dijk", "Henderson", "Mane", "Salah"], "Klopp")
+    team.result("lose")
+    assert_equal(0, team.points)
+  end
+
+  def test_points__draw
+    team = Team.new("Liverpool", ["Karius", "van Dijk", "Henderson", "Mane", "Salah"], "Klopp")
+    team.result("draw")
+    assert_equal(1, team.points)
   end
 
 end
--- part_b_sports_team/sports_team.rb
+++ part_b_sports_team/sports_team.rb
@@ -1,25 +1,46 @@
 class Team
 
-  def initialize(name, players, coach)
+  attr_reader(:name, :players, :coach, :points)
+
+  attr_writer(:coach)
+
+  def initialize(name, players, coach, points = 0)
     @name = name
     @players = players
     @coach = coach
+    @points = points
   end
 
-  def get_name
-    return @name
+  # def get_name
+  #   return @name
+  # end
+  #
+  # def get_players
+  #   return @players
+  # end
+  #
+  # def get_coach
+  #   return @coach
+  # end
+
+  # def set_coach(new_coach)
+  #   @coach = new_coach
+  # end
+
+  def add_new_player(new_player)
+    @players.push(new_player)
   end
 
-  def get_players
-    return @players
+  def player_in_team?(possible_player)
+    for player in @players
+      return true if player == possible_player
+    end
+    return false
   end
 
-  def get_coach
-    return @coach
-  end
-
-  def set_coach(new_coach)
-    @coach = new_coach
+  def result(result)
+    @points += 3 if result == "win"
+    @points += 1 if result == "draw"
   end
 
 end
